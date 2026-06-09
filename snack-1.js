e89340cfe25c5cd2075dc050f89228fa73b6ebf7
paolo-della-fera <paolodf2005@gmail.com>
2026-06-09 13:04:25 +0200

    first commit

---
+++ snack-1.js
@@ -0,0 +1,14 @@
+// Funzione Dichiarativa
+function somma (numero1, numero2) {
+    return numero1 + numero2
+}
+
+
+// Funzione Anonima assegnata a una variabile
+const somma = function (numero1, numero2) {
+    return numero1 + numero2
+} 
+
+
+// Arroe Function 
+const somma = (numero1, numero2) => numero1 + numero2
